c17636712e7861cf3ca13fd0460b3c266954221f
Benjamin Auquite <halomastar@gmail.com>
2025-12-13 20:30:24 -0600

    fix: use Installation.DetermineGame in SpoofKotorRegistry

--- src/CSharpKOTOR/Tools/Registry.cs
+++ src/CSharpKOTOR/Tools/Registry.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using Microsoft.Win32;
 using CSharpKOTOR.Common;
-using CSharpKOTOR.Installation;
 using CSharpKOTOR.Logger;
 
 namespace CSharpKOTOR.Tools
