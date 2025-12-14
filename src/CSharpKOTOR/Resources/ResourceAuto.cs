4614f2abd2a2b237e492bcaf208723a53117de8e
Benjamin Auquite <halomastar@gmail.com>
2025-12-13 19:06:42 -0600

    feat: enhance ResourceAuto with ReadResource and ResourceToBytes
    - Add ReadResource method for automatic resource reading from various sources
    - Add ReadUnknownResource method for format detection
    - Add ResourceToBytes method for converting resources to bytes
    - Support all major resource formats (TLK, SSF, 2DA, TPC, ERF, RIM, GFF, NCS, MDL, VIS, LYT, LTR, BWM, LIP)
    - Matching PyKotor implementation at Libraries/PyKotor/src/pykotor/resource/resource_auto.py

--- src/CSharpKOTOR/Resources/ResourceAuto.cs
+++ src/CSharpKOTOR/Resources/ResourceAuto.cs
@@ -1,26 +1,345 @@
 using System;
 using System.IO;
 using CSharpKOTOR.Common;
+using CSharpKOTOR.Formats.BWM;
 using CSharpKOTOR.Formats.ERF;
 using CSharpKOTOR.Formats.GFF;
+using CSharpKOTOR.Formats.LIP;
+using CSharpKOTOR.Formats.LTR;
+using CSharpKOTOR.Formats.LYT;
+using CSharpKOTOR.Formats.MDL;
+using CSharpKOTOR.Formats.MDLData;
+using CSharpKOTOR.Formats.NCS;
 using CSharpKOTOR.Formats.RIM;
 using CSharpKOTOR.Formats.SSF;
 using CSharpKOTOR.Formats.TLK;
+using CSharpKOTOR.Formats.TPC;
 using CSharpKOTOR.Formats.TwoDA;
-using CSharpKOTOR.Resource.Formats.LYT;
-using CSharpKOTOR.Resource.Formats.VIS;
+using CSharpKOTOR.Formats.VIS;
 using CSharpKOTOR.Resource.Generics;
 using JetBrains.Annotations;
 
 namespace CSharpKOTOR.Resources
 {
-    /// <summary>
-    /// Automatic resource loading and saving utilities.
-    /// Provides automatic detection and loading of different resource types.
-    /// </summary>
+    // Matching PyKotor implementation at Libraries/PyKotor/src/pykotor/resource/resource_auto.py
+    // Original: Automatic resource loading and saving utilities
     [PublicAPI]
     public static class ResourceAuto
     {
+        // Matching PyKotor implementation at Libraries/PyKotor/src/pykotor/resource/resource_auto.py:58-123
+        // Original: def read_resource(source: SOURCE_TYPES, resource_type: ResourceType | None = None) -> bytes:
+        public static byte[] ReadResource(object source, ResourceType resourceType = null)
+        {
+            string sourcePath = null;
+            try
+            {
+                if (source is string path)
+                {
+                    sourcePath = path;
+                    if (resourceType == null)
+                    {
+                        var resId = ResourceIdentifier.FromPath(path);
+                        resourceType = resId.ResType;
+                    }
+                }
+            }
+            catch
+            {
+                // Ignore errors in path detection
+            }
+
+            if (resourceType == null)
+            {
+                return ReadUnknownResource(source);
+            }
+
+            try
+            {
+                string ext = resourceType.Extension?.ToLowerInvariant() ?? "";
+                string resourceExt = ext.StartsWith(".") ? ext.Substring(1) : ext;
+
+                if (resourceType.Category == "Talk Tables")
+                {
+                    TLK tlk = source is string s ? TLKAuto.ReadTlk(s) : TLKAuto.ReadTlk(source as byte[]);
+                    return TLKAuto.BytesTlk(tlk);
+                }
+                if (resourceType == ResourceType.TGA || resourceType == ResourceType.TPC)
+                {
+                    TPC tpc = TPCAuto.ReadTpc(source);
+                    return TPCAuto.BytesTpc(tpc);
+                }
+                if (resourceExt == "ssf")
+                {
+                    SSF ssf = source is string s2 ? SSFAuto.ReadSsf(s2) : SSFAuto.ReadSsf(source as byte[]);
+                    return SSFAuto.BytesSsf(ssf);
+                }
+                if (resourceExt == "2da")
+                {
+                    byte[] data2da = source is string s3 ? File.ReadAllBytes(s3) : source as byte[];
+                    var reader2da = new TwoDABinaryReader(data2da);
+                    TwoDA twoda = reader2da.Load();
+                    return TwoDAAuto.BytesTwoDA(twoda);
+                }
+                if (resourceExt == "lip")
+                {
+                    LIP lip = LIPAuto.ReadLip(source);
+                    return LIPAuto.BytesLip(lip);
+                }
+                if (ResourceType.FromExtension(resourceExt) == ResourceType.ERF || 
+                    ResourceType.FromExtension(resourceExt) == ResourceType.MOD || 
+                    ResourceType.FromExtension(resourceExt) == ResourceType.SAV)
+                {
+                    ERF erf = source is string s4 ? ERFAuto.ReadErf(s4) : ERFAuto.ReadErf(source as byte[]);
+                    return ERFAuto.BytesErf(erf);
+                }
+                if (resourceExt == "rim")
+                {
+                    RIM rim = source is string s5 ? RIMAuto.ReadRim(s5) : RIMAuto.ReadRim(source as byte[]);
+                    return RIMAuto.BytesRim(rim);
+                }
+                if (resourceType.Extension?.ToUpperInvariant() == "GFF" || GFFContentExtensions.Contains(resourceType.Extension?.ToUpperInvariant() ?? ""))
+                {
+                    byte[] dataGff = source is string s6 ? File.ReadAllBytes(s6) : source as byte[];
+                    var readerGff = new GFFBinaryReader(dataGff);
+                    GFF gff = readerGff.Load();
+                    return GFFAuto.BytesGff(gff, ResourceType.GFF);
+                }
+                if (resourceExt == "ncs")
+                {
+                    NCS ncs = source is string s7 ? NCSAuto.ReadNcs(s7) : NCSAuto.ReadNcs(source as byte[]);
+                    return NCSAuto.BytesNcs(ncs);
+                }
+                if (resourceExt == "mdl")
+                {
+                    MDLData.MDL mdl = MDLAuto.ReadMdl(source);
+                    using (var ms = new MemoryStream())
+                    {
+                        MDLAuto.WriteMdl(mdl, ms);
+                        return ms.ToArray();
+                    }
+                }
+                if (resourceExt == "vis")
+                {
+                    VIS vis = VISAuto.ReadVis(source);
+                    return VISAuto.BytesVis(vis);
+                }
+                if (resourceExt == "lyt")
+                {
+                    LYT lyt = LYTAuto.ReadLyt(source);
+                    return LYTAuto.BytesLyt(lyt);
+                }
+                if (resourceExt == "ltr")
+                {
+                    LTR ltr = LTRAuto.ReadLtr(source);
+                    return LTRAuto.BytesLtr(ltr);
+                }
+                if (resourceType.Category == "Walkmeshes")
+                {
+                    BWM bwm = source is string s8 ? BWMAuto.ReadBwm(s8) : BWMAuto.ReadBwm(source as byte[]);
+                    return BWMAuto.BytesBwm(bwm);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException($"Could not load resource '{sourcePath}' as resource type '{resourceType}': {e.Message}", e);
+            }
+
+            throw new ArgumentException($"Resource type {resourceType} is not supported by this library.");
+        }
+
+        // Matching PyKotor implementation at Libraries/PyKotor/src/pykotor/resource/resource_auto.py:126-158
+        // Original: def read_unknown_resource(source: SOURCE_TYPES) -> bytes:
+        public static byte[] ReadUnknownResource(object source)
+        {
+            try
+            {
+                TLK tlk = source is string s ? TLKAuto.ReadTlk(s) : TLKAuto.ReadTlk(source as byte[]);
+                return TLKAuto.BytesTlk(tlk);
+            }
+            catch { }
+
+            try
+            {
+                SSF ssf = source is string s2 ? SSFAuto.ReadSsf(s2) : SSFAuto.ReadSsf(source as byte[]);
+                return SSFAuto.BytesSsf(ssf);
+            }
+            catch { }
+
+            try
+            {
+                byte[] data2da = source is string s3 ? File.ReadAllBytes(s3) : source as byte[];
+                var reader2da = new TwoDABinaryReader(data2da);
+                TwoDA twoda = reader2da.Load();
+                return TwoDAAuto.BytesTwoDA(twoda);
+            }
+            catch { }
+
+            try
+            {
+                LIP lip = LIPAuto.ReadLip(source);
+                return LIPAuto.BytesLip(lip);
+            }
+            catch { }
+
+            try
+            {
+                TPC tpc = TPCAuto.ReadTpc(source);
+                return TPCAuto.BytesTpc(tpc);
+            }
+            catch { }
+
+            try
+            {
+                ERF erf = source is string s4 ? ERFAuto.ReadErf(s4) : ERFAuto.ReadErf(source as byte[]);
+                return ERFAuto.BytesErf(erf);
+            }
+            catch { }
+
+            try
+            {
+                RIM rim = source is string s5 ? RIMAuto.ReadRim(s5) : RIMAuto.ReadRim(source as byte[]);
+                return RIMAuto.BytesRim(rim);
+            }
+            catch { }
+
+            try
+            {
+                NCS ncs = source is string s6 ? NCSAuto.ReadNcs(s6) : NCSAuto.ReadNcs(source as byte[]);
+                return NCSAuto.BytesNcs(ncs);
+            }
+            catch { }
+
+            try
+            {
+                byte[] dataGff = source is string s7 ? File.ReadAllBytes(s7) : source as byte[];
+                var readerGff = new GFFBinaryReader(dataGff);
+                GFF gff = readerGff.Load();
+                return GFFAuto.BytesGff(gff, ResourceType.GFF);
+            }
+            catch { }
+
+            try
+            {
+                MDLData.MDL mdl = MDLAuto.ReadMdl(source);
+                using (var ms = new MemoryStream())
+                {
+                    MDLAuto.WriteMdl(mdl, ms);
+                    return ms.ToArray();
+                }
+            }
+            catch { }
+
+            try
+            {
+                VIS vis = VISAuto.ReadVis(source);
+                return VISAuto.BytesVis(vis);
+            }
+            catch { }
+
+            try
+            {
+                LYT lyt = LYTAuto.ReadLyt(source);
+                return LYTAuto.BytesLyt(lyt);
+            }
+            catch { }
+
+            try
+            {
+                LTR ltr = LTRAuto.ReadLtr(source);
+                return LTRAuto.BytesLtr(ltr);
+            }
+            catch { }
+
+            try
+            {
+                BWM bwm = source is string s8 ? BWMAuto.ReadBwm(s8) : BWMAuto.ReadBwm(source as byte[]);
+                return BWMAuto.BytesBwm(bwm);
+            }
+            catch { }
+
+            throw new ArgumentException("Source resource data not recognized as any kotor file formats.");
+        }
+
+        // Matching PyKotor implementation at Libraries/PyKotor/src/pykotor/resource/resource_auto.py:210-243
+        // Original: def resource_to_bytes(resource: ...) -> bytes:
+        public static byte[] ResourceToBytes(object resource)
+        {
+            if (resource is ARE || resource is DLG || resource is GIT || resource is IFO || 
+                resource is JRL || resource is PTH || resource is UTC || resource is UTD || 
+                resource is UTE || resource is UTM || resource is UTP || resource is UTS || 
+                resource is UTW)
+            {
+                // GFF generics - would need dismantle methods
+                throw new NotImplementedException("Dismantle methods for GFF generics not yet implemented");
+            }
+            if (resource is BWM bwm)
+            {
+                return BWMAuto.BytesBwm(bwm);
+            }
+            if (resource is GFF gff)
+            {
+                return GFFAuto.BytesGff(gff, ResourceType.GFF);
+            }
+            if (resource is ERF erf)
+            {
+                return ERFAuto.BytesErf(erf);
+            }
+            if (resource is LIP lip)
+            {
+                return LIPAuto.BytesLip(lip);
+            }
+            if (resource is LTR ltr)
+            {
+                return LTRAuto.BytesLtr(ltr);
+            }
+            if (resource is LYT lyt)
+            {
+                return LYTAuto.BytesLyt(lyt);
+            }
+            if (resource is MDLData.MDL mdl)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    MDLAuto.WriteMdl(mdl, ms);
+                    return ms.ToArray();
+                }
+            }
+            if (resource is NCS ncs)
+            {
+                return NCSAuto.BytesNcs(ncs);
+            }
+            if (resource is RIM rim)
+            {
+                return RIMAuto.BytesRim(rim);
+            }
+            if (resource is SSF ssf)
+            {
+                return SSFAuto.BytesSsf(ssf);
+            }
+            if (resource is TLK tlk)
+            {
+                return TLKAuto.BytesTlk(tlk);
+            }
+            if (resource is TPC tpc)
+            {
+                return TPCAuto.BytesTpc(tpc);
+            }
+            if (resource is TwoDA twoda)
+            {
+                return TwoDAAuto.BytesTwoDA(twoda);
+            }
+            if (resource is VIS vis)
+            {
+                return VISAuto.BytesVis(vis);
+            }
+
+            throw new ArgumentException($"Invalid resource {resource} of type '{resource.GetType().Name}' passed to ResourceToBytes.");
+        }
+
+        private static readonly System.Collections.Generic.HashSet<string> GFFContentExtensions = new System.Collections.Generic.HashSet<string>
+        {
+            "ARE", "IFO", "GIT", "UTC", "UTI", "UTD", "UTE", "UTP", "UTS", "UTT", "UTW", "DLG", "JRL", "PTH"
+        };
         /// <summary>
         /// Automatically loads a resource from file data based on its type.
         /// </summary>
